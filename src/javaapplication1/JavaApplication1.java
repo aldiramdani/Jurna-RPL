155c91ab02acdc2023af04a0d855fa249745d6a9
Praktikan <Praktikan@Praktikan-PC>
2018-03-27 10:45:26 +0700

    test

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -18,8 +18,8 @@ public class JavaApplication1 {
         System.out.print("Masukan Jumlah Deret Fibonacci ");
         int n = show.nextInt();
         long fib[] = new long[n];
-         
-        fib[0] = 0;
+         long x =0;
+        fib[0] = 1;
         fib[1] = 1;
          
         for(int i = 2; i < n; i++) {
@@ -27,8 +27,9 @@ public class JavaApplication1 {
         }
          
         for (int i = 0; i < n; i++) {
-            System.out.print(fib[i] +  " ");
+             x = x + fib[i];   
         }
-    
+        System.out.print("Output " +x);
+            
     }
 }
